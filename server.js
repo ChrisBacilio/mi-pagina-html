608beaef0fed67d4dfb16645ea6858dcf4004e66
ChrisBacilio <christophercuentaescuela@gmail.com>
2024-11-17 17:15:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,10 @@ app.post('/reservar', (req, res) => {
     });
 });
 
-// Usar el puerto dinámico proporcionado por Render, o 3000 si no está disponible
+// Usar el puerto proporcionado por Render, o 3000 si no está disponible
 const PORT = process.env.PORT || 3000;
 
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
-
